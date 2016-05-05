b484befd2c8032cce666ab37e9225a6eec85599e
raleigh wayland <rtwayland@gmail.com>
2016-05-05 14:34:23 -0600

    Designating Calculation php and display

--- php_survey/calc_survey_results.php
+++ php_survey/calc_survey_results.php
@@ -119,3 +119,95 @@ $file = fopen('results.txt', 'w');
   fclose($file);
 
  ?>
+
+ <!DOCTYPE html>
+ <html>
+
+ <head>
+     <meta charset="utf-8">
+     <title>Survey Results</title>
+     <link rel="stylesheet" href="survey.css" type="text/css">
+ </head>
+
+ <body>
+     <div class="results-wrapper">
+         <h1>Survey Results</h1>
+         <p>Digital Systems should be required for Software Engineering majors:</p>
+         <table class="results-table">
+             <th>Yes</th>
+             <th>No</th>
+             <tr>
+                 <td>
+                     <?php echo $yes160_count; ?>
+                 </td>
+                 <td>
+                     <?php echo $no160_count; ?>
+                 </td>
+             </tr>
+         </table>
+         <p>C++ should be the first programming language learned:</p>
+         <table class="results-table">
+             <th>Yes</th>
+             <th>No</th>
+             <tr>
+                 <td>
+                     <?php echo $yes_cpp_count; ?>
+                 </td>
+                 <td>
+                     <?php echo $no_cpp_count; ?>
+                 </td>
+             </tr>
+         </table>
+
+         <p>Which programming language should be taught as a student's first language?</p>
+         <table class="results-table">
+             <th>C++</th>
+             <th>Java</th>
+             <th>Python</th>
+             <th>JavaScript</th>
+             <tr>
+                 <td>
+                     <?php echo $cpp_lang; ?>
+                 </td>
+                 <td>
+                     <?php echo $java_lang; ?>
+                 </td>
+                 <td>
+                     <?php echo $python_lang; ?>
+                 </td>
+                 <td>
+                     <?php echo $javascript_lang; ?>
+                 </td>
+             </tr>
+         </table>
+
+         <p>Which of these options would you like to see added as an emphasis in the Computer Science department?</p>
+         <table class="results-table">
+             <th>User Experience Design</th>
+             <th>Web Development</th>
+             <th>Game Development</th>
+             <th>Artificial Inteligence</th>
+             <th>Other</th>
+             <tr>
+                 <td>
+                     <?php echo $ux; ?>
+                 </td>
+                 <td>
+                     <?php echo $webDev; ?>
+                 </td>
+                 <td>
+                     <?php echo $gDev; ?>
+                 </td>
+                 <td>
+                     <?php echo $ai; ?>
+                 </td>
+                 <td>
+                     <?php echo $other; ?>
+                 </td>
+             </tr>
+         </table>
+         <?php echo $comment ?>
+     </div>
+ </body>
+
+ </html>
