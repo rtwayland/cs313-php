ffbf6352fbbc633ef94a9fbc36dd270deb9ee6a2
raleigh wayland <rtwayland@gmail.com>
2016-05-21 10:50:52 -0600

    "Updating the loader for OpenShift"

--- bathroom_guide/database_loader.php
+++ bathroom_guide/database_loader.php
@@ -1,18 +1,30 @@
 <?php
+
 function loadDatabase()
 {
-    $dbHost = 'localhost';
-    $dbUser = 'bath_app_user';
-    $dbPassword = 'pass';
+    $dbHost = '';
+    $dbPort = '';
+    $dbUser = '';
+    $dbPassword = '';
 
-    $dbName = 'bathroom_guide';
-    try {
-        $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
-    } catch (Exception $e) {
-        echo $e;
-        die();
+    $dbName = '';
+
+    $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+    if ($openShiftVar === null || $openShiftVar == '') {
+        // Not in the openshift environment
+        //echo "Using local credentials: ";
+        require 'setLocalDatabaseCredentials.php';
+    } else {
+        // In the openshift environment
+        //echo "Using openshift credentials: ";
+
+        $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+        $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+        $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+        $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
     }
+    $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
 
     return $db;
 }
-?>
